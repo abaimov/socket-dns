4663b02f863782a20bedf54183f38411e556ddba
rickmoralis <megapowerbob@gmail.com>
2024-10-22 13:24:01 +0300

    add index.html

--- app.mjs
+++ app.mjs
@@ -1,12 +1,24 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import dotenv from 'dotenv'; // Импортируем dotenv
+
+dotenv.config(); // Инициализация dotenv
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-app.get("/", (req, res) => res.send('WS'));
+// Получаем __dirname для ES-модулей
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+});
 
 io.on('connection', (socket) => {
     console.log('Новое подключение: ', socket.id);
@@ -23,6 +35,8 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(8080, () => {
-    console.log('Сервер запущен на порту 8080');
+// Используйте переменную PORT из файла .env
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
 });
