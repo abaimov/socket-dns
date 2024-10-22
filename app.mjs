1f509d9a6720d1a5c482e6a9bd598e91389c1fdb
rickmoralis <megapowerbob@gmail.com>
2024-10-22 12:45:05 +0300

    mjs and json

--- app.mjs
+++ app.mjs
@@ -1,20 +1,23 @@
-const express = require('express');
-const http = require('http');
-const {Server} = require('socket.io');
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
-const server = http.createServer(app);
+const server = createServer(app);
 const io = new Server(server);
 
 app.get("/", (req, res) => res.send('WS'));
 
 io.on('connection', (socket) => {
     console.log('Новое подключение: ', socket.id);
+
     socket.on('message', (msg) => {
         console.log('Получено сообщение: ', msg);
         socket.emit('response', 'Сообщение получено сервером');
     });
+
     socket.emit('welcome', 'Соединение установлено');
+
     socket.on('disconnect', () => {
         console.log('Клиент отключился');
     });
