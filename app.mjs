751a8b7450e1bcdd610c35c25aa1003b0c940447
rickmoralis <megapowerbob@gmail.com>
2024-10-22 14:19:39 +0300

    edit console.log time

--- app.mjs
+++ app.mjs
@@ -1,6 +1,6 @@
 import express from 'express';
 import { createServer } from 'http';
-import { WebSocketServer } from 'ws'; // Исправленный импорт
+import { WebSocketServer } from 'ws';
 import { join } from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -10,36 +10,36 @@ dotenv.config();
 
 const app = express();
 const server = createServer(app);
-const wss = new WebSocketServer({ server }); // Создаем экземпляр WebSocket.Server
+const wss = new WebSocketServer({ server });
 
-// Получаем __dirname для ES-модулей
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const getCurrentTime = () => {
+    const now = new Date();
+    return now.toTimeString().split(' ')[0];
+};
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
 });
 
-// Обработка WebSocket соединений
 wss.on('connection', (ws) => {
-    console.log('Новое подключение');
+    console.log(`[${getCurrentTime()}] Новое подключение`);
 
-    // Отправляем приветственное сообщение
     ws.send('Соединение установлено');
 
-    // Обработка входящих сообщений
     ws.on('message', (message) => {
-        console.log('Получено сообщение: ', message);
+        console.log(`[${getCurrentTime()}] Получено сообщение: `, message);
         ws.send('Сообщение получено сервером');
     });
 
-    // Обработка отключения клиента
     ws.on('close', () => {
-        console.log('Клиент отключился');
+        console.log(`[${getCurrentTime()}] Клиент отключился`);
     });
 });
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
-    console.log(`Сервер запущен на порту ${PORT}`);
+    console.log(`[${getCurrentTime()}] Сервер запущен на порту ${PORT}`);
 });
